d234d5c25164da14e1d85eb02e0aa0881d875d2d
Osama Hassanein <player1os@gmail.com>
2017-05-12 18:21:50 +0200

    Partial commit

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-var BuffPanelSdkTrack = function (gameToken) {
-	var gaAll = null;
-	if (ga && ga.getAll) {
-		gaAll = ga.getAll();
-	} else {
-		return;
-	}
-	var trackingId = null;
-	var clientId = null;
-	if ((gaAll.length >= 1) && gaAll[0] && gaAll[0].get) {
-		clientId = gaAll[0].get('clientId');
-		trackingId = gaAll[0].get('trackingId');
-	} else {
-		return;
-	}
+var BuffPanelSdk = function (_window, _document) {
+	var _class = function (gameToken) {
+		var gaAll = null;
+		if (ga && ga.getAll) {
+			gaAll = ga.getAll();
+		} else {
+			return;
+		}
+		var trackingId = null;
+		var clientId = null;
+		if ((gaAll.length >= 1) && gaAll[0] && gaAll[0].get) {
+			clientId = gaAll[0].get('clientId');
+			trackingId = gaAll[0].get('trackingId');
+		} else {
+			return;
+		}
+	};
 
-	var image = document.createElement('img');
-	image.onload = function() {};
-	image.src = 'http://buffpanel.com/api/ga_event/' + gameToken + '?tid=' + trackingId + '&cid=' + clientId;
-};
+	return new 
+		
+
+		var image = document.createElement('img');
+		image.onload = function() {};
+		image.src = 'http://buffpanel.com/api/ga_event/' + gameToken + '?tid=' + trackingId + '&cid=' + clientId;
+	};
+}(window, document);
