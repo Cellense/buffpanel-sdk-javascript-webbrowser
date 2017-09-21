bad67b0ec55022cfb4800e8c04b47995409b91a2
Osama Hassanein <osama.hassanein@cellense.com>
2017-09-21 13:10:19 +0200

    Reorganize the project

--- src/loader.js
+++ src/loader.js
@@ -1,3 +1,3 @@
-(function(b,u,f,p,a,n,e,l){b[a]=b[a]||function(type,data){(b[a].q=b[a].q||[]).push({type:type,data:data});};e=u.createElement(f);e.async=1;e.src=p;l=u.getElementsByTagName(f)[0];l.parentNode.insertBefore(e,l);})(window,document,'script','https://cellense.com/websdk/sdk.js','BuffPanelSdk');
+(function(b,u,f,p,a,n,e,l){b[a]=b[a]||function(type,data){(b[a].q=b[a].q||[]).push({type:type,data:data});};e=u.createElement(f);e.async=1;e.src=p;l=u.getElementsByTagName(f)[0];l.parentNode.insertBefore(e,l);})(window,document,'script','https://buffpanel.com/web_sdk/sdk.js','BuffPanelSdk');
 BuffPanelSdk('initialize', '');
 BuffPanelSdk('sendTrackingData');
